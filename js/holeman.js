41f215e719a0b8fca78b50980c3a8ea18a09dc7d
totttt <leeyj0177@gmail.com>
2017-03-23 16:35:35 +0900

    holeman went center

--- js/holeman.js
+++ js/holeman.js
@@ -1,13 +1,5 @@
-//background
-    var background = new Path.Rectangle({
-        point: [0, 0],
-        size: [view.size.width, view.size.height],
-        fillColor: '#0000ff',
-        });
-
-
 //fill
-    var head = new Path({
+    var fill = new Path({
         segments: [[300,200], [308,180], [315,150], [325,110], [335,90], [350,83], [365,90], [375,110], [385,150], [392,180], [400,200], [410,207], [420,210], [480,220], [520,230], [531,240], [531,243], [527,248], [519,252], [480,257], [450,260], [445,270], [445,430], [442,490], [437,530], [430,540], [420,543], [410,540], [403,530], [398,490], [395,450], [390,437], [350,430], [310,437], [305,450], [302,490], [297,530], [290,540], [280,543], [270,540], [263,530], [258,490], [255,430], [255,270], [250,260], [220,257], [180,252], [172,248], [169,243], [169,240], [180,230], [220,220], [280,210], [290,207], [300,200]],
         closed: false,
         fillColor: '#ffffff',
@@ -16,7 +8,7 @@
         strokeCap: 'round'
         });
 
-    head.smooth({
+    fill.smooth({
         type: 'catmull-rom',
         factor: 0.4
         });
@@ -51,6 +43,9 @@
         strokeCap: 'round'
         });
 
+    var decor = new Group([
+        decor_1, decor_2, decor_3, decor_4
+        ])
 
 //head
     var head = new Path({
@@ -90,6 +85,9 @@
         strokeCap: 'round'
         });
 
+    var face = new Group([
+        head, l_eye, r_eye, mouth
+        ])
 
 //arms
     var l_arm = new Path({
@@ -194,3 +192,14 @@
         type: 'catmull-rom',
         factor: 0.4
         });
+
+        var body = new Group([
+        l_body, r_body, hole, inner_hole, l_leg, r_leg, l_arm, r_arm
+        ])
+
+//group
+    var holeman = new Group([
+        fill, decor, body, face
+        ])
+
+    holeman.position=view.center
