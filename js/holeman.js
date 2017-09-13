9c7b489ee2b7d8b64fcac0913840b366cfaef1f8
totttt <leeyj0177@gmail.com>
2017-09-13 12:06:24 +0900

    update_002

--- js/holeman.js
+++ js/holeman.js
@@ -517,7 +517,7 @@ function onFrame(event) {
         // singing_mouth_5.size = [12,singing_mouth_size/2];
     }
     else {
-        song_lyrics_0.position.x -= 2;
+        // song_lyrics_0.position.x -= 2;
         // song_lyrics_1.position.x -= 2;
         // song_lyrics_2.position.x -= 2;
         // song_lyrics_3.position.x -= 2;
