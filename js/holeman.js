10e73ce03b514942af2cd536875424adcc88ac76
totttt <leeyj0177@gmail.com>
2017-09-13 23:50:28 +0900

    update_020

--- js/holeman.js
+++ js/holeman.js
@@ -508,7 +508,7 @@ function onFrame(event) {
     count++;
     if (count % 4 === 0) {
         var singing_mouth_size = getRandom(10, 40);
-        singing_mouth.position.y = view.viewSize.height-615+singing_mouth_size/2;
+        singing_mouth.position.y = view.viewSize.height-620+singing_mouth_size/2;
         singing_mouth.size = [30,singing_mouth_size];
         // singing_mouth_2.position.y = view.viewSize.height-195+singing_mouth_size/4;
         // singing_mouth_2.size = [12,singing_mouth_size/2];
